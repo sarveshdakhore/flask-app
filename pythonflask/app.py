6b05f6b41fcde96453fc2dfdbbd9af857815c928
Satyajeet Narayan <56536469+Satyajeet-code@users.noreply.github.com>
2024-06-27 22:05:42 +0530

    app.py

--- pythonflask/app.py
+++ pythonflask/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 import datetime
 import psycopg2
-import database
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired
@@ -48,7 +47,7 @@ def signup():
             password=form.password.data
             form.name.data=" "
             print("--------------------------------------------------- name:\n ", name)
-            status= database.insert(name, email,phone,password,dt)
+           
 
 
 
@@ -67,16 +66,12 @@ def login():
             email=loginform.email.data
             password=loginform.password.data
             
-            status= database.fetch(email,password)
+            
 
         else:
     # Access validation errors
           for field, errors in loginform.errors.items():
               print(f"Error in field {field}: {errors}")
-        if status=="Data feteched successfully!":
-          return render_template("login.html",email=email,password=password,task="/tasks",form=loginform)
-        else:
-           return render_template("login.html",email=email,password=password,status=status,form=loginform)
         
     return render_template("login.html",email=email,password=password,form=loginform)
 
